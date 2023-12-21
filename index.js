df00007550bbbe0dbb9a2d1d70262fd41fc4248f
Gisan999 <ruheatheking3@gmail.com>
2023-12-22 03:42:45 +0600

    delete operation successfully working

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -16,10 +15,7 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json())
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.csnq8lx.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -34,15 +30,15 @@ async function run() {
 
 
         app.post('/set/tasks', async (req, res) => {
-            const newTask = req.body;
-            const result = await tasksCollection.insertOne(newTask);
+            const nweData = req.body;
+            const result = await tasksCollection.insertOne(nweData);
             res.send(result);
         })
 
         app.get('/get/tasks', async (req, res) => {
             const email = req.query.email;
-            const query = { email: email };
-            const result = await tasksCollection.find(query).sort({ sorting: 1 }).toArray();
+            const filter = { email: email };
+            const result = await tasksCollection.find(filter).sort({ sorting: 1 }).toArray();
             res.send(result);
         })
 
@@ -71,7 +67,6 @@ async function run() {
             const result = await tasksCollection.deleteOne(query);
             res.send(result);
         })
-
 
 
         await client.db("admin").command({ ping: 1 });
