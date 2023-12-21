ac229390ee16649c4c70cd86d5d38f126eb41af7
Gisan999 <ruheatheking3@gmail.com>
2023-12-22 03:20:36 +0600

    todo list task update operation complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -21,37 +21,63 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    const tasksCollection = client.db('tasksList').collection('tasks');
+    try {
+        const tasksCollection = client.db('tasksList').collection('tasks');
 
 
-    app.post('/set/tasks', async (req, res) => {
-        const newTask = req.body;
-        const result = await tasksCollection.insertOne(newTask);
-        res.send(result);
-    })
+        app.post('/set/tasks', async (req, res) => {
+            const newTask = req.body;
+            const result = await tasksCollection.insertOne(newTask);
+            res.send(result);
+        })
 
-    app.get('/get/tasks', async (req, res) => {
-        const email = req.query.email;
-        const query = {email: email};
-        const result = await tasksCollection.find(query).sort({ sorting: 1 }).toArray();
-        res.send(result);
-    })
+        app.get('/get/tasks', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await tasksCollection.find(query).sort({ sorting: 1 }).toArray();
+            res.send(result);
+        })
+
+
+        app.put('/update/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateTask = req.body;
+            const task = {
+                $set: {
+                    title: updateTask.title,
+                    description: updateTask.description,
+                    deadline: updateTask.deadline,
+                    priority: updateTask.priority,
+                    sorting: updateTask.sorting
+                }
+            };
+            const result = await tasksCollection.updateOne(filter, task, options);
+            res.send(result);
+        })
+
+        app.delete('/delete/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await tasksCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-  }
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
 }
 run().catch(console.dir);
 
